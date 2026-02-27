d92081818907ebf14314902cc21d5d71c00b3d15
Rajasekhar50 <mallelarajasekharyadav50@gmail.com>
2026-02-27 21:47:48 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int numbers[] = {10, 20, 30, 40, 50}; 
+    
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    printf("Elements in the array are:\\n");
+
+    for (int i = 0; i < size; i++) {
+        printf("Element %d = %d\\n", i, numbers[i]);
+    }
+
+    return 0;
+}
