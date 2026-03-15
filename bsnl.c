e0ebfa160cf7c1e2acfe0dd0c4b20a490949800d
Rajasekhar50 <mallelarajasekharyadav50@gmail.com>
2026-03-15 21:24:35 +0530

    Add files via upload

---
+++ bsnl.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("BSNL Services Codes:\n");
+    printf("Balance & Validity: *123#\n");
+    printf("Offers & Plans: *124#\n");
+    printf("Check Your Number: *1# or *888#\n");
+    printf("VAS Services: *789#\n");
+    return 0;
+}
